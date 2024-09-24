632b47dfd7a5836de51c41350d49a8e81d1ef803
ogabekcoder <mengniyozovogabek01@gmail.com>
2024-09-24 15:41:51 +0500

    kril => lotin

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -4,11 +4,90 @@ import AuthorMiddleware from "../middleware/owner.middleware.js"
 
 const router = express.Router()
 
-function generateSlug(title) {
+function transliterate(title) {
+    const translitMap = {
+        а: "a",
+        б: "b",
+        в: "v",
+        г: "g",
+        д: "d",
+        е: "e",
+        ё: "yo",
+        ж: "zh",
+        з: "z",
+        и: "i",
+        й: "y",
+        к: "k",
+        л: "l",
+        м: "m",
+        н: "n",
+        о: "o",
+        п: "p",
+        р: "r",
+        с: "s",
+        т: "t",
+        у: "u",
+        ф: "f",
+        х: "kh",
+        ц: "ts",
+        ч: "ch",
+        ш: "sh",
+        щ: "shch",
+        ъ: "",
+        ы: "y",
+        ь: "",
+        э: "e",
+        ю: "yu",
+        я: "ya",
+        А: "A",
+        Б: "B",
+        В: "V",
+        Г: "G",
+        Д: "D",
+        Е: "E",
+        Ё: "Yo",
+        Ж: "Zh",
+        З: "Z",
+        И: "I",
+        Й: "Y",
+        К: "K",
+        Л: "L",
+        М: "M",
+        Н: "N",
+        О: "O",
+        П: "P",
+        Р: "R",
+        С: "S",
+        Т: "T",
+        У: "U",
+        Ф: "F",
+        Х: "Kh",
+        Ц: "Ts",
+        Ч: "Ch",
+        Ш: "Sh",
+        Щ: "Shch",
+        Ъ: "",
+        Ы: "Y",
+        Ь: "",
+        Э: "E",
+        Ю: "Yu",
+        Я: "Ya",
+    }
+
     return title
+        .split("")
+        .map((char) => translitMap[char] || char)
+        .join("")
+}
+
+function generateSlug(title) {
+    // Kirilcha harflarni lotin harflariga o'tkazish
+    const transliteratedTitle = transliterate(title)
+
+    return transliteratedTitle
         .toLowerCase() // Kichik harflarga o'zgartirish
         .trim() // Bosh va oxiridagi bo'shliqlarni olib tashlash
-        .replace(/[^a-zA-Z0-9а-яА-ЯёЁ\s-]/g, "") // Maxsus belgilarni olib tashlash (faqat harflar, raqamlar, bo'shliqlar va chiziqlar qoldiriladi)
+        .replace(/[^a-zA-Z0-9\s-]/g, "") // Maxsus belgilarni olib tashlash
         .replace(/\s+/g, "-") // Bo'shliqlarni chiziqcha bilan almashtirish
 }
 
